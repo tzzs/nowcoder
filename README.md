4e8400ea292c9648bf834f0ed1d671092e474bd5
tzzs <1789803837@qq.com>
2019-04-04 21:17:58 +0800

    AC 二叉树的镜像 JAVA

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
-# nowcoder
+# NOWCODER
+
+
+## 在线编程
+
+剑指Offer：https://www.nowcoder.com/ta/coding-interviews
---
+++ 剑指Offer/树的子结构/JAVA.java
@@ -0,0 +1,35 @@
+import javax.swing.tree.TreeNode;
+
+/**
+ * Definition for a binary tree node. public class TreeNode { int val; TreeNode
+ * left; TreeNode right; TreeNode(int x) { val = x; } }
+ */
+class Solution {
+    public boolean isSubtree(TreeNode s, TreeNode t) {
+        if (s == null && t == null) {
+            return true;
+        } else if (s == null || t == null) {
+            return false;
+        } else {
+            if (s.val == t.val) {
+                return isSameTree(s, t);
+            } else {
+                return isSubtree(s.left, t) || isSubtree(s.right, t);
+            }
+        }
+    }
+
+    public boolean isSameTree(TreeNode s, TreeNode t) {
+        if (s == null && t == null) {
+            return true;
+        } else if (s == null || t == null) {
+            return false;
+        } else {
+            if (s.val == t.val) {
+                return isSameTree(s.left, t.left)||isSameTree(s.right, t.right);
+            } else {
+                return false;
+            }
+        }
+    }
+}
