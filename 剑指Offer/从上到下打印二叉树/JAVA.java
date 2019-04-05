3b2b9ae085517fe59e1225c51949b5fce670ba24
tzzs <1789803837@qq.com>
2019-04-06 03:10:23 +0800

    AC 从上到下打印二叉树 JAVA

---
+++ 剑指Offer/从上到下打印二叉树/JAVA.java
@@ -0,0 +1,37 @@
+import java.util.ArrayList;
+import java.util.Queue;
+import java.util.LinkedList;
+/**
+public class TreeNode {
+    int val = 0;
+    TreeNode left = null;
+    TreeNode right = null;
+
+    public TreeNode(int val) {
+        this.val = val;
+
+    }
+
+}
+*/
+public class Solution {
+    public ArrayList<Integer> PrintFromTopToBottom(TreeNode root) {
+        Queue<TreeNode> queue = new LinkedList<TreeNode>();
+        ArrayList<Integer> list = new ArrayList<>();
+        if (root == null) {
+            return list;
+        }
+        queue.offer(root);
+        while (queue.peek() != null) {
+            TreeNode node = queue.poll();
+            list.add(node.val);
+            if (node.left != null) {
+                queue.offer(node.left);
+            }
+            if (node.right != null) {
+                queue.offer(node.right);
+            }
+        }
+        return list;
+    }
+}
